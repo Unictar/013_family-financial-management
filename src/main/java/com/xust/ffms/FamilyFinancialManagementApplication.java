b84bc4dd69b80ef0b1e57dc79d110b836b744b08
蔡奕彤 <2939673768@qq.com>
2026-03-21 14:00:00 +0800

    docs: 初始化项目仓库，添加.gitignore和README

---
+++ src/main/java/com/xust/ffms/FamilyFinancialManagementApplication.java
@@ -0,0 +1,13 @@
+package com.xust.ffms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyFinancialManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyFinancialManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xust/ffms/FamilyFinancialManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xust.ffms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyFinancialManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
